804ec539956cbed06b031f98daae8888e3f6d658
erikr93 <erikr93@gmail.com>
2017-09-11 15:35:43 -0700

    cleaned up gameboard trying to make hand cards clickable now

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
-$.getJSON('cards.json', function(data) {
-  console.log(data);
+var cards = $.getJSON('../data.json', function(data) {
 });
 
 function randomizeCards(){
-	for(i = 0; )
-}
+	var object = JSON.parse(cards);
+	console.log(object)
+	$(".handCard").click(function(){
+		console.log("clicked");
+		for(var i = 0; i < object.length; i++){
+			var card = Math.floor(Math.random() * 52);
+			$(".startCard").append('img')[i];
+		}
+	});		
+}	
