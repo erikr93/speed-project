7818bea25787d9642f5f714582feedede17abe63
erikr93 <erikr93@gmail.com>
2017-09-11 10:51:47 -0700

    set up the card object and divs with no styling

---
+++ js/script.js
@@ -0,0 +1,261 @@
+var cards = [
+{
+	rank: 2,
+	suit: "clubs",
+	img: url("img/2_of_clubs.png")
+},
+{
+	rank: 2,
+	suit:"diamonds",
+	img: url("img/2_of_diamonds.png")
+},
+{
+	rank: 2,
+	suit: "hearts",
+	img: url("img/2_of_hearts.png")
+},
+{
+	rank: 2,
+	suit: "spades",
+	img: url("img/2_of_spades.png")
+},
+{
+	rank: 3,
+	suit: "clubs",
+	img: url("img/3_of_clubs.png")
+},
+{
+	rank: 3,
+	suit:"diamonds",
+	img: url("img/3_of_diamonds.png")
+},
+{
+	rank: 3,
+	suit: "hearts",
+	img: url("img/3_of_hearts.png")
+},
+{
+	rank: 3,
+	suit: "spades",
+	img: url("img/3_of_spades.png")
+},
+{
+	rank: 4,
+	suit: "clubs",
+	img: url("img/4_of_clubs.png")
+},
+{
+	rank: 4,
+	suit: "diamonds",
+	img: url("img/4_of_diamonds.png")
+},
+{
+	rank: 4,
+	suit: "hearts",
+	img: url("img/4_of_hearts.png")
+},
+{
+	rank: 4,
+	suit: "spades",
+	img: url("img/4_of_spades.png")
+},
+{
+	rank: 5,
+	suit: "clubs",
+	img: url("img/5_of_clubs.png")
+},
+{
+	rank: 5,
+	suit: "diamonds",
+	img: url("img/5_of_diamonds.png")
+},
+{
+	rank: 5,
+	suit: "hearts",
+	img: url("img/5_of_hearts.png")
+},
+{
+	rank: 5,
+	suit: "spades",
+	img: url("img/5_of_spades.png")
+},
+{
+	rank: 6,
+	suit: "clubs",
+	img: url("img/6_of_clubs.png")
+},
+{
+	rank: 6,
+	suit: "diamonds",
+	img: url("img/6_of_diamonds.png")
+},
+{
+	rank: 6,
+	suit: "hearts",
+	img: url("img/6_of_hearts.png")
+},
+{
+	rank: 6,
+	suit: "spades",
+	img: url("img/6_of_spades.png")
+},
+{
+	rank: 7,
+	suit: "clubs",
+	img: url("img/7_of_clubs.png")
+},
+{
+	rank: 7,
+	suit: "diamonds",
+	img: url("img/7_of_diamonds.png")
+},
+{
+	rank: 7,
+	suit: "hearts",
+	img: url("img/7_of_hearts.png")
+},
+{
+	rank: 7,
+	suit: "spades",
+	img: url("img/7_of_spades.png")
+},
+{
+	rank: 8,
+	suit: "clubs",
+	img: url("img/8_of_clubs.png")
+},
+{
+	rank: 8,
+	suit: "diamonds",
+	img: url("img/8_of_diamonds.png")
+},
+{
+	rank: 8,
+	suit: "hearts",
+	img: url("img/8_of_hearts.png")
+},
+{
+	rank: 8,
+	suit: "spades",
+	img: url("img/8_of_spades.png")
+},
+{
+	rank: 9,
+	suit: "clubs",
+	img: url("img/9_of_clubs.png")
+},
+{
+	rank: 9,
+	suit: "diamonds",
+	img: url("img/9_of_diamonds.png")
+},
+{
+	rank: 9,
+	suit: "hearts",
+	img: url("img/9_of_hearts.png")
+},
+{
+	rank: 9,
+	suit: "spades",
+	img: url("img/9_of_spades.png")
+},
+{
+	rank: 10,
+	suit: "clubs",
+	img: url("img/10_of_clubs.png")
+},
+{
+	rank: 10,
+	suit: "diamonds",
+	img: url("img/10_of_diamonds.png")
+},
+{
+	rank: 10,
+	suit: "hearts",
+	img: url("img/10_of_hearts.png")
+},
+{
+	rank: 10,
+	suit: "spades",
+	img: url("img/10_of_spades.png")
+},
+{
+	rank: 11,
+	suit: "clubs",
+	img: url("img/jack_of_clubs2.png")
+},
+{
+	rank: 11,
+	suit: "diamonds",
+	img: url("img/jack_of_diamonds2.png")
+},
+{
+	rank: 11,
+	suit: "hearts",
+	img: url("img/jack_of_hearts2.png")
+},
+{
+	rank: 11,
+	suit: "spades",
+	img: url("img/jack_of_spades2.png")
+},
+{
+	rank: 12,
+	suit: "clubs",
+	img: url("img/queen_of_clubs2.png")
+},
+{
+	rank: 12,
+	suit: "diamonds",
+	img: url("img/queen_of_diamonds2.png")
+},
+{
+	rank: 12,
+	suit: "hearts",
+	img: url("img/queen_of_hearts2.png")
+},
+{
+	rank: 12,
+	suit: "spades",
+	img: url("img/queen_of_spades2.png")
+},
+{
+	rank: 13,
+	suit: "clubs",
+	img: url("img/king_of_clubs2.png")
+},
+{
+	rank: 13,
+	suit: "diamonds",
+	img: url("img/king_of_diamonds2.png")
+},
+{
+	rank: 13,
+	suit: "hearts",
+	img: url("img/king_of_hearts2.png")
+},
+{
+	rank: 13,
+	suit: "spades",
+	img: url("img/king_of_spades2.png")
+},
+{
+	rank: 1,
+	suit: "clubs",
+	img: url("img/ace_of_clubs.png")
+},
+{
+	rank: 1,
+	suit: "diamonds",
+	img: url("img/ace_of_diamonds.png")
+},
+{
+	rank: 1,
+	suit: "hearts",
+	img: url("img/ace_of_hearts.png")
+},
+{
+	rank: 1,
+	suit: "spades",
+	img: url("img/ace_of_spades.png")
+}]
